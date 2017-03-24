8b0bc51fdc13558184988809693bfb1ca5e6148d
Marianna <mangat.marianna@gmail.com>
2017-03-24 01:34:30 +0000

    Solution

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,9 +1,21 @@
 """This is the entry point of the program."""
 
 
+# def highest_number_cubed(limit):
+#     num = 1
+#     while num <= limit:
+#         num = num **3
+#         num += 1
+        
+#     return limit
+        
 def highest_number_cubed(limit):
-    num = 1
-    while limit > num:
-        num = num **3
-    return limit
+    prenum = 1
+    
+    while True:
+        nownum = prenum +1
+        if nownum **3 > limit:
+            return prenum
+            
+        prenum = nownum
         
